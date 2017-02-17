fa337170a36b583a78d9def125ad9cdb1fb70c52
Pawel Fiuto <pawel@bashton.com>
2017-02-17 16:30:25 +0000

    removed all ASG/S3 stuff

--- 4_listeners/variables.tf
+++ 4_listeners/variables.tf
@@ -1,7 +1,6 @@
-// Global Configuration
+# Global Configuration
 variable "name" {}
 variable "envname" {}
-variable "envtype" {}
 variable "service" {}
 
 variable "region" {
@@ -26,20 +25,16 @@ variable "elb_aws_account" {
   }
 }
 
-// DNS Variables
+# DNS Variables
 variable "elb_dns" {
   default = "0"
 }
 
-variable "route53_zone_id" {
-  default = "changeme"
-}
+variable "route53_zone_id" {}
 
-variable "domain" {
-  default = "example.com"
-}
+variable "domain" {}
 
-// ELB Configuration Variables
+# ELB Configuration Variables
 variable "availability_zones" {
   type = "list"
 }
@@ -158,110 +153,4 @@ variable "log_s3prefix" {
 
 variable "log_interval" {
   default = "60"
-}
-
-# ASG Configuration Variables
-variable "ami_id" {}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "iam_instance_profile" {
-  default = ""
-}
-
-variable "security_groups" {
-  type = "list"
-}
-
-variable "associate_public_ip_address" {
-  default = false
-}
-
-variable "detailed_monitoring" {
-  default = true
-}
-
-variable "user_data" {
-  default = ""
-}
-
-variable "subnets" {
-  type = "list"
-}
-
-variable "min" {
-  default = 1
-}
-
-variable "max" {
-  default = 1
-}
-
-variable "autoscaling" {
-  default = false
-}
-
-variable "cpu_scale_up" {
-  default = "60"
-}
-
-variable "scale_minutes_up" {
-  default = "5"
-}
-
-variable "cpu_scale_down" {
-  default = "20"
-}
-
-variable "scale_minutes_down" {
-  default = "20"
-}
-
-variable "scale_factor_up" {
-  default = "1"
-}
-
-variable "scale_factor_down" {
-  default = "-1"
-}
-
-variable "scale_statistic" {
-  default = "Maximum"
-}
-
-variable "cooldown" {
-  default = 300
-}
-
-variable "key_name" {
-  default = "bashton"
-}
-
-variable "termination_policies" {
-  type = "list"
-
-  default = [
-    "OldestLaunchConfiguration",
-    "ClosestToNextInstanceHour",
-  ]
-}
-
-variable "health_check_type" {
-  default = "EC2"
-}
-
-variable "health_check_grace_period" {
-  default = 300
-}
-
-variable "load_balancers" {
-  type    = "list"
-  default = []
-}
-
-variable "extra_elbs" {
-  type    = "list"
-  default = []
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# Global Configuration
 variable "name" {}
 variable "envname" {}
 variable "service" {}
@@ -29,13 +30,9 @@ variable "elb_dns" {
   default = "0"
 }
 
-variable "route53_zone_id" {
-  default = "changeme"
-}
+variable "route53_zone_id" {}
 
-variable "domain" {
-  default = "example.com"
-}
+variable "domain" {}
 
 # ELB Configuration Variables
 variable "availability_zones" {
@@ -96,110 +93,4 @@ variable "log_s3prefix" {
 
 variable "log_interval" {
   default = "60"
-}
-
-# ASG Configuration Variables
-variable "ami_id" {}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "iam_instance_profile" {
-  default = ""
-}
-
-variable "security_groups" {
-  type = "list"
-}
-
-variable "associate_public_ip_address" {
-  default = false
-}
-
-variable "detailed_monitoring" {
-  default = true
-}
-
-variable "user_data" {
-  default = ""
-}
-
-variable "subnets" {
-  type = "list"
-}
-
-variable "min" {
-  default = 1
-}
-
-variable "max" {
-  default = 1
-}
-
-variable "autoscaling" {
-  default = false
-}
-
-variable "cpu_scale_up" {
-  default = "60"
-}
-
-variable "scale_minutes_up" {
-  default = "5"
-}
-
-variable "cpu_scale_down" {
-  default = "20"
-}
-
-variable "scale_minutes_down" {
-  default = "20"
-}
-
-variable "scale_factor_up" {
-  default = "1"
-}
-
-variable "scale_factor_down" {
-  default = "-1"
-}
-
-variable "scale_statistic" {
-  default = "Maximum"
-}
-
-variable "cooldown" {
-  default = 300
-}
-
-variable "key_name" {
-  default = "bashton"
-}
-
-variable "termination_policies" {
-  type = "list"
-
-  default = [
-    "OldestLaunchConfiguration",
-    "ClosestToNextInstanceHour",
-  ]
-}
-
-variable "health_check_type" {
-  default = "EC2"
-}
-
-variable "health_check_grace_period" {
-  default = 300
-}
-
-variable "load_balancers" {
-  type    = "list"
-  default = []
-}
-
-variable "extra_elbs" {
-  type    = "list"
-  default = []
 }
