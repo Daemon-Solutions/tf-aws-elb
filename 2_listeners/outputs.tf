042e2de2293c1b84c2198c5037119277e869b5fd
Liam Bennett <liam@bashton.com>
2017-06-09 13:39:24 +0100

    removing unused outputs

--- 2_listeners/outputs.tf
+++ 2_listeners/outputs.tf
@@ -1,12 +1,4 @@
-output "launch_config_id" {
-    value = "${aws_launch_configuration.lc.id}"
-}
-output "asg_id" {
-    value = "${aws_autoscaling_group.asg.id}"
-}
-output "asg_name" {
-    value = "${aws_autoscaling_group.asg.name}"
-}
+
 output "elb_log_s3bucket" {
     value = "${aws_s3_bucket.elb_log_bucket.id}"
 }
