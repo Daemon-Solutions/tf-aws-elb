5f09e0d0d6e0d45b9fe3f1aa9e50baa2a48083d7
Liam Bennett <liam@bashton.com>
2017-06-09 13:57:03 +0100

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
-output "elb_log_s3bucket" {
-    value = "${aws_s3_bucket.elb_log_bucket.id}"
-}
+
 output "elb_dns_name" {
     value = "${aws_elb.elb.dns_name}"
 }
+
 output "elb_zone_id" {
     value = "${aws_elb.elb.zone_id}"
 }
